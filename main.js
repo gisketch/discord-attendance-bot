3b47921c82a02a8e29ca1c66b510d057f54f7d76
gisketch <agjmnz@gmail.com>
2021-03-11 19:17:45 +0800

    discord bot init

---
+++ main.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const prefix = "a!";
+
+client.once("ready", () => {
+  console.log("AoTTG 2 Attendance is ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("ODE5NTI1OTMyNjE5NjYxMzQz.YEn5AA.7Q-NYkBK32wNnhNsUXAoKHGYd-E");
