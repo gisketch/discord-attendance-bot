8ac79836024ed748be5867135169152fc007de23
gisketch <agjmnz@gmail.com>
2021-03-12 20:31:40 +0800

    bugfix

--- commands/data.js
+++ commands/data.js
@@ -4,12 +4,16 @@ module.exports = {
     name: 'data',
     description: 'Check data files',
     async execute(client, message, args, Discord) {
-        let files = fs.readdirSync(`./db/`);
-        let embed = new Discord.MessageEmbed()
-            .setTitle('Data check!')
-            .setColor('#AAEEAA')
-            .setDescription('Here are the available data to check.')
-            .addFields({ name: 'Data', value: files.join('\n') });
-        await message.channel.send(embed);
+        try {
+            let files = fs.readdirSync(`./db/`);
+            let embed = new Discord.MessageEmbed()
+                .setTitle('Data check!')
+                .setColor('#AAEEAA')
+                .setDescription('Here are the available data to check.')
+                .addFields({ name: 'Data', value: files.join('\n') });
+            await message.channel.send(embed);
+        } catch (err) {
+            console.log(err);
+        }
     },
 };
