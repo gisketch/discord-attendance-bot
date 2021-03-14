212f61a9d942c5cc0c2ff92f24fb755d36f12961
gisketch <agjmnz@gmail.com>
2021-03-14 16:39:36 +0800

    Update monthly.js

--- commands/monthly.js
+++ commands/monthly.js
@@ -323,7 +323,7 @@ module.exports = {
                 if (args[1] === 'art') {
                     if (args[2]) {
                         const artFiltered = artResult[artPage - 1].filter((r) =>
-                            r.replace('<@', '').replace('>', '')
+                            console.log(r)
                         );
                         if (args[2] === '2d') {
                             console.log(artFiltered);
