d3745571aa88bac8935c5a323fa3e2b4601e27ce
gisketch <agjmnz@gmail.com>
2021-03-14 16:42:07 +0800

    Update monthly.js

--- commands/monthly.js
+++ commands/monthly.js
@@ -323,10 +323,10 @@ module.exports = {
                 if (args[1] === 'art') {
                     if (args[2]) {
                         const artFiltered = artResult[artPage - 1].filter((r) =>
-                            console.log(r.replace(/\D/g, ''))
+                            r.replace(/\D/g, '')
                         );
                         if (args[2] === '2d') {
-                            console.log('logged');
+                            console.log(artFiltered);
                         }
                     }
 
