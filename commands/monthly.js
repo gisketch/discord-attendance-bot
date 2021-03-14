5e9ea41473a1bb764a9b0e933dfd7c11ef8af9bc
gisketch <agjmnz@gmail.com>
2021-03-14 16:30:12 +0800

    Update monthly.js

--- commands/monthly.js
+++ commands/monthly.js
@@ -321,6 +321,15 @@ module.exports = {
 
                 // Role checks for the user
                 if (args[1] === 'art') {
+                    if (args[2]) {
+                        const artFiltered = artResult.filter((r) =>
+                            r.replace(/\D/g, '')
+                        );
+                        if (args[2] === '2d') {
+                            console.log(artFiltered);
+                        }
+                    }
+
                     attendanceEmbed
                         .setTitle(
                             `Art Team Attendance Check for ${args[0]}-2021`
